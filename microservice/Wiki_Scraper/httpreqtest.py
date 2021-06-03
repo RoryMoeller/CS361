ceebcbe323c1f675e2d408bb4f0a4b8f1dccb22d
Rory S Moeller <moellerr@oregonstate.edu>
2021-06-03 15:10:20 -0700

    Removing old comments

--- microservice/Wiki_Scraper/httpreqtest.py
+++ microservice/Wiki_Scraper/httpreqtest.py
@@ -1,5 +1,4 @@
 import requests
 from sys import argv
-# req = requests.post(url = "http://localhost:" + argv[1] + "/saveimgs/https://en.wikipedia.org/wiki/System")
 req = requests.get(url = "http://localhost:" + argv[1] + "/get/https://en.wikipedia.org/wiki/System")
 print(req.text)
